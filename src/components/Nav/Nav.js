34b890de8a3f9d73bc9093f78ef214cd215824b7
wrrrg <bill.gryta@gmail.com>
2018-03-26 07:45:19 -0500

    starting to update style

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,7 +3,7 @@ import "./Nav.css";
 
 const Nav = props => (
 <div className="score-nav">
-  <h1 className="title">Click React</h1>
+  <h1 className="title">Saturday Morning Cartoons</h1>
   <h2 className="message">{props.gameText}</h2>
   <h2 className="scores"> Current Score: {props.currentScore} | Top Score: {props.topScore}</h2>
 </div>
